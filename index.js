3946b9ba89a0ef9ec40e8853639c3db558f8c1df
krae <kraewind@gmail.com>
2021-04-28 11:26:25 -0400

    commit tree fccff1e7a27639db9b2490ea10ae96af01ec32e2

--- index.js
+++ index.js
@@ -10,4 +10,23 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+
+function greet(time) {
+  let ary = time.split(":")
+  debugger
+  if (parseInt(ary[0]) < 12) {
+    return "Good Morning"
+  }
+  else if (12 <= parseInt(ary[0]) && parseInt(ary[0]) <= 16) {
+    return "Good Afternoon"
+  }
+  else {
+    return "Good Evening"
+  }
+}
+
 /* Write your implementation of displayMessage() */
+
+function displayMessage(arg) {
+  document.querySelector("#greeting").innerText = arg
+}
